8a73ba3151a59ff627796e06d7ed09603b2a8869
khaddy06 <harameedey06@gmail.com>
2025-07-21 16:33:05 +0100

    fix responsiveness

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,14 +4,14 @@ function Home() {
   return (
     <div className="bg-[#0a192f]">
       <div
-        className="w-full max-w-screen-xl px-4 sm:px-6 lg:px-20 mx-auto pt-16 sm:pt-20
+        className="w-full max-w-screen-xl px-4 px-6 lg:px-20 mx-auto pt-16 sm:pt-20
        md:pt-24 pb-12 sm:pb-16 md:pb-20"
       >
-        <h2 className="text-base sm:text-lg md:text-xl lg:text-2xl font-bold text-[#64ffda]">
+        <h2 className="md:text-2xl text-xl font-bold text-[#64ffda]">
           Hi, my name is
         </h2>
-        <h1 className="text-7xl font-bold text-[#ccd6f6]">Sanni Khadijah.</h1>
-        <h2 className="text-6xl text-[#8892b0] mt-2 font-bold w-2xl">
+        <h1 className="md:text-7xl text-5xl font-bold text-[#ccd6f6]">Sanni Khadijah.</h1>
+        <h2 className="md:text-6xl text-5xl text-[#8892b0] mt-2 font-bold md:w-2xl">
           I build elegant websites & web apps.
         </h2>
         <p className="text-[#ccd6f6] max-w-xl mt-4 text-xl">
