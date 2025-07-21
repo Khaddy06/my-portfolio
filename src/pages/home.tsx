0b923e52d3e9c17a810e21f19d4d5b2019a5efba
khaddy06 <harameedey06@gmail.com>
2025-07-21 16:47:24 +0100

    responsiveness

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,8 +4,8 @@ function Home() {
   return (
     <div className="bg-[#0a192f]">
       <div
-        className="w-full max-w-screen-xl px-4 px-6 lg:px-20 mx-auto pt-16 sm:pt-20
-       md:pt-24 pb-12 sm:pb-16 md:pb-20"
+        className="w-full max-w-screen-xl px-6 lg:px-20 mx-auto pt-20
+       md:pt-24  pb-16 md:pb-20"
       >
         <h2 className="md:text-2xl text-xl font-bold text-[#64ffda]">
           Hi, my name is
@@ -14,13 +14,14 @@ function Home() {
         <h2 className="md:text-6xl text-5xl text-[#8892b0] mt-2 font-bold md:w-2xl">
           I build elegant websites & web apps.
         </h2>
-        <p className="text-[#ccd6f6] max-w-xl mt-4 text-xl">
+        <p className="text-[#ccd6f6] md:max-w-xl mt-4 md:text-xl text-base sm:text-center lg:text-left">
           I’m a front-end developer focused on crafting smooth, responsive, and
           accessible digital experiences — blending aesthetic with logic.
         </p>
         <a
           href="#contact"
-          className="mt-6 inline-block border border-[#64ffda] text-[#64ffda] px-6 py-2 rounded hover:shadow-[0_0_10px_#64ffda] 
+          className="mt-6 inline-block border border-[#64ffda] text-[#64ffda] font-bold text-base
+          px-6 py-2 rounded hover:shadow-[0_0_10px_#64ffda] 
           transition-all duration-300"
         >
           Get in touch
--- src/tabs/upStatement.tsx
+++ src/tabs/upStatement.tsx
@@ -3,14 +3,14 @@ import { MdOutlinePlayArrow } from "react-icons/md";
 function UpStatement() {
   return (
     <div>
-      <h2 className="text-xl text-[#ccd6f6] font-title  pb-2">
+      <h2 className="md:text-xl text-lg text-[#ccd6f6] font-semibold  pb-2">
         Lead Engineer <span className="text-[#64ffda] mx-1">@</span>
         <span className="text-[#64ffda] hover:underline">Upstatement</span>
       </h2>
       <p className="text-[#ccd6f6] text-sm font-light">May 2018 - Present</p>
       <div className="flex mt-5 ">
         <MdOutlinePlayArrow size={16} color="#64ffda" />
-        <p className="text-md text-[#8892b0] font-semibold ml-1 w-lg">
+        <p className="text-md text-[#8892b0] font-semibold ml-1 md:w-lg  ">
           Deliver High-quality, robust production code for a diverse array of
           projects for clients including Harvard Business Schools, Everytown for
           Gun Safety , Pratt Institute, koala Health, Vanderblit University, The
